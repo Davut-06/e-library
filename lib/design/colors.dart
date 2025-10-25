796c09523045d01e2b167cb58073ef42627a07c0
Davut-06 <davut.ahmedov.0621@gmail.com>
2025-10-25 13:34:14 +0500

    first commit

---
+++ lib/design/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/rendering.dart';
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFF4570EA);
+
+const Color secondaryColor = Color(0xFF2A3547);
+
+const Color secondaryVariantColor = Color(0xFF5A6A85);
+
+const Color backgroundColor = Color(0xFFFFFFFF);
+
+const Color textPrimaryColor = Color(0xFF7C8FAC);
+
+const Color iconColor = Color(0xFFC8D7F1);
